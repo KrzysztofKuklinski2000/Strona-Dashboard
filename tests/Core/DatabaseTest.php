ed297f4d85eaf6bac6bd71f981a9f7823b8e55a3
KrzysztofKuKliński <zono.mrzono21@gmail.com>
2026-01-23 13:48:13 +0100

    test(core): add tests for Database connection
    * verified that RepositoryException is thrown when connection fails
    * verified that successful connection returns valid PDO instance

---
+++ tests/Core/DatabaseTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Core;
+
+use PDO;
+use App\Core\Database;
+use PHPUnit\Framework\TestCase;
+use App\Exception\RepositoryException;
+
+class DatabaseTest extends TestCase {
+  public function testShouldThrowRepositoryExceptionWhenConnectionFails(): void
+  {
+    // GIVEN
+    $config = [
+      'database' => 'not_existing_db',
+      'host' => 'invalid_host',
+      'user' => 'user',
+      'password' => 'pass'
+    ];
+
+    $database = new Database($config);
+
+    // EXPECTS
+    $this->expectException(RepositoryException::class);
+    $this->expectExceptionMessage('Błąd połączenia z bazą danych !');
+
+    // WHEN
+    $database->connect();
+  }
+
+  public function testShouldReturnPdoObjectWhenCredentialsAreValid(): void
+  {
+    // GIVEN
+    $config =  [
+      'host' => 'localhost',
+      'database' => 'karate',
+      'user' => 'user_karate',
+      'password' => 'Qwerty1@3'
+    ];
+
+    $database = new Database($config);
+
+    // WHEN
+    $pdo = $database->connect();
+
+    // THEN
+    $this->assertInstanceOf(PDO::class, $pdo);
+  }
+}
