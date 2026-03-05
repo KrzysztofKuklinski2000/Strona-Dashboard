e2fe437f5b452b3603621d21a0890450ad1d8d84
KrzysztofKuklinski2000 <zono.mrzono21@gmail.com>
2026-03-05 17:34:17 +0100

    style: fix code formatting in SubscriberController

--- src/Controller/Dashboard/SubscribersController.php
+++ src/Controller/Dashboard/SubscribersController.php
@@ -1,5 +1,4 @@
-<?php 
-
+<?php
 
 namespace App\Controller\Dashboard;
 
@@ -9,28 +8,30 @@ use App\View;
 use App\Middleware\CsrfMiddleware;
 use App\Service\Dashboard\SubscribersManagementServiceInterface;
 
-class SubscribersController extends AbstractDashboardController {
-  public function __construct(
-    private SubscribersManagementServiceInterface $service,
-    Request $request,
-    EasyCSRF $easyCSRF,
-    View $view,
-    CsrfMiddleware $csrfMiddleware
-    ){
-    
-    parent::__construct($request, $easyCSRF, $service, $view, $csrfMiddleware);
-  }
+class SubscribersController extends AbstractDashboardController
+{
+    public function __construct(
+        private SubscribersManagementServiceInterface $service,
+        Request $request,
+        EasyCSRF $easyCSRF,
+        View $view,
+        CsrfMiddleware $csrfMiddleware
+    ) {
 
-  public function indexAction(): void
-  {
-    $this->renderPage([
-      'page' => 'subscribers/index',
-      'data' => $this->service->getAllSubscribers(),
-    ]);
-  }
+        parent::__construct($request, $easyCSRF, $service, $view, $csrfMiddleware);
+    }
 
-  protected function getModuleName(): string
-  {
-    return 'subscribers';
-  }
+    public function indexAction(): void
+    {
+        $this->renderPage([
+          'page' => 'subscribers/index',
+          'data' => $this->service->getAllSubscribers(),
+        ]);
+    }
+
+    protected function getModuleName(): string
+    {
+        return 'subscribers';
+    }
 }
+
