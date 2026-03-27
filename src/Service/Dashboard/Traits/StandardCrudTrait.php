515e8c862ca8ba16877d9d96ed093bec645aafbd
KrzysztofKuklinski2000 <zono.mrzono21@gmail.com>
2026-03-27 22:21:59 +0100

    create StandardCrudTrait

--- src/Service/Dashboard/Traits/StandardCrudTrait.php
+++ src/Service/Dashboard/Traits/StandardCrudTrait.php
@@ -2,6 +2,30 @@
 
 namespace App\Service\Dashboard\Traits;
 
-trait StandardCrudTrait {
+use App\Exception\RepositoryException;
+use App\Exception\ServiceException;
 
+trait StandardCrudTrait {
+    protected function edit(string $table, array $data): void
+    {
+        try {
+            $this->repository->edit($table, $data);
+        } catch (RepositoryException $e) {
+            throw new ServiceException("Błąd edycji", 500, $e);
+        }
+    }
+
+    protected function published(string $table, array $data): void
+    {
+        try {
+            $this->repository->published($table, $data);
+        } catch (RepositoryException $e) {
+            throw new ServiceException("Błąd zmiany statusu", 500, $e);
+        }
+    }
+
+    protected function simpleDelete(string $table, int $id): void
+    {
+        $this->execute(fn() => $this->repository->delete($id, $table), "Nie udało się usunąć");
+    }
 }
