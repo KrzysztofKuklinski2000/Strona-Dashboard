4752cefcdbffd0f1631419c1d78f8680dd7fa61a
KrzysztofKuklinski2000 <zono.mrzono21@gmail.com>
2026-04-14 17:44:04 +0200

    refactor: update TimetableController to use modular action traits
    Applied new architecture to TimetableController:
    
    * Integrated HasStoreAction, HasDeleteAction, HasUpdateAction, HasPublishedAction, and HasMoveAction.

--- src/Controller/Dashboard/TimetableController.php
+++ src/Controller/Dashboard/TimetableController.php
@@ -2,87 +2,116 @@
 
 namespace App\Controller\Dashboard;
 
+use App\Controller\Dashboard\Traits\HasDeleteAction;
+use App\Controller\Dashboard\Traits\HasPublishedAction;
+use App\Controller\Dashboard\Traits\HasStoreAction;
+use App\Controller\Dashboard\Traits\HasUpdateAction;
 use App\Core\Request;
+use App\Exception\NotFoundException;
 use App\Middleware\CsrfMiddleware;
 use App\Service\Dashboard\TimetableManagementServiceInterface;
 use App\View;
 
-class TimetableController extends AbstractDashboardController {
+class TimetableController extends AbstractDashboardController
+{
+    use HasStoreAction, HasPublishedAction, HasUpdateAction, HasDeleteAction;
 
-  public function __construct(
-    private TimetableManagementServiceInterface $service,
-    Request $request,
-    View $view,
-    CsrfMiddleware $csrfMiddleware
-  ) {
-    parent::__construct($request, $service, $view, $csrfMiddleware);
-  }
+    public function __construct(
+        private readonly TimetableManagementServiceInterface $service,
+        Request                                              $request,
+        View                                                 $view,
+        CsrfMiddleware                                       $csrfMiddleware
+    )
+    {
+        parent::__construct($request, $service, $view, $csrfMiddleware);
+    }
 
-  public function indexAction(): void {
-    $this->renderPage([
-      'page' => 'timetable/index',
-      'data' => $this->service->getAllTimetable(),
-    ]);
-  }
+    public function indexAction(): void
+    {
+        $this->renderPage([
+            'page' => 'timetable/index',
+            'data' => $this->service->getAllTimetable(),
+        ]);
+    }
 
-  public function editAction(): void {
-    $this->renderPage([
-      'page' => 'timetable/edit',
-      'data' => $this->getSingleData(),
-    ]);
-  }
+    /**
+     * @throws NotFoundException
+     */
+    public function editAction(): void
+    {
+        $this->renderPage([
+            'page' => 'timetable/edit',
+            'data' => $this->getSingleData(),
+        ]);
+    }
 
-  public function createAction(): void {
-    $this->renderPage([
-      'page' => 'timetable/create',
-    ]);
-  }
+    public function createAction(): void
+    {
+        $this->renderPage([
+            'page' => 'timetable/create',
+        ]);
+    }
 
-  public function showAction(): void {
-    $this->renderPage([
-      'page' => 'timetable/show',
-      'data' => $this->getSingleData(),
-    ]);
-  }
+    /**
+     * @throws NotFoundException
+     */
+    public function showAction(): void
+    {
+        $this->renderPage([
+            'page' => 'timetable/show',
+            'data' => $this->getSingleData(),
+        ]);
+    }
 
-  public function confirmDeleteAction(): void {
-    $this->renderPage([
-      'page' => 'timetable/delete',
-      'data' => $this->getSingleData(),
-    ]);
-  }
+    /**
+     * @throws NotFoundException
+     */
+    public function confirmDeleteAction(): void
+    {
+        $this->renderPage([
+            'page' => 'timetable/delete',
+            'data' => $this->getSingleData(),
+        ]);
+    }
 
-  protected function getModuleName(): string {
-    return 'timetable';
-  }
+    protected function getModuleName(): string
+    {
+        return 'timetable';
+    }
 
-  protected function getDataToCreate(): array {
-    return $this->getDataToAddTimetable();
-  }
+    protected function getDataToCreate(): array
+    {
+        return $this->getDataToAddTimetable();
+    }
 
-  protected function getDataToUpdate(): array {
-    return $this->getDataToEditTimetable();
-  }
+    protected function getDataToUpdate(): array
+    {
+        return $this->getDataToEditTimetable();
+    }
 
-  protected function getDataToPublished()
-  {
-    return $this->getDataToPublishedTimetable();
-  }
+    protected function getDataToPublished(): array
+    {
+        return $this->getDataToPublishedTimetable();
+    }
 
-  protected function handleCreate(array $data): void {
-    $this->service->createTimetable($data);
-  }
+    protected function handleCreate(array $data): void
+    {
+        $this->service->createTimetable($data);
+    }
 
-  protected function handleUpdate(array $data): void {
-    $this->service->updateTimetable($data);
-  }
+    protected function handleUpdate(array $data): void
+    {
+        $this->service->updateTimetable($data);
+    }
 
-  protected function handleDelete(int $id): void {
-    $shouldNotify = !empty($this->request->getFormParam('is_notify'));
-    $this->service->deleteTimetable($id, $shouldNotify);
-  }
+    protected function handleDelete(int $id): void
+    {
+        $shouldNotify = !empty($this->request->getFormParam('is_notify'));
+        $this->service->deleteTimetable($id, $shouldNotify);
+    }
 
-  protected function handlePublish(array $data): void {
-    $this->service->publishedTimetable($data);
-  }
+    protected function handlePublish(array $data): void
+    {
+        $this->service->publishedTimetable($data);
+    }
 }
