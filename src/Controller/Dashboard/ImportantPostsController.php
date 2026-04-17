45ddc30cf5c7752b9868de9ae1681b62fb16fb63
KrzysztofKuklinski2000 <zono.mrzono21@gmail.com>
2026-04-17 17:54:47 +0200

    refactor: update ImportantPostsController to use modular action traits
    Applied new architecture to ImportantPostsController:
    
    * Integrated HasStoreAction, HasDeleteAction, HasUpdateAction, HasPublishedAction, and HasMoveAction.

--- src/Controller/Dashboard/ImportantPostsController.php
+++ src/Controller/Dashboard/ImportantPostsController.php
@@ -2,84 +2,117 @@
 
 namespace App\Controller\Dashboard;
 
+use App\Controller\Dashboard\Traits\HasDeleteAction;
+use App\Controller\Dashboard\Traits\HasMoveAction;
+use App\Controller\Dashboard\Traits\HasPublishedAction;
+use App\Controller\Dashboard\Traits\HasStoreAction;
+use App\Controller\Dashboard\Traits\HasUpdateAction;
+use App\Exception\NotFoundException;
 use App\View;
 use App\Core\Request;
 use App\Middleware\CsrfMiddleware;
 use App\Service\Dashboard\ImportantPostsManagementServiceInterface;
 
-class ImportantPostsController extends AbstractDashboardController {
-  public function __construct(
-    public ImportantPostsManagementServiceInterface $service,
-    Request $request,
-    View $view,
-    CsrfMiddleware $csrfMiddleware
-  ) {
-    parent::__construct($request, $service, $view, $csrfMiddleware);
-  }
+class ImportantPostsController extends AbstractDashboardController
+{
 
-  public function indexAction(): void {
-    $this->renderPage([
-      'page' => 'important_posts/index',
-      'data' => $this->service->getAllImportantPosts(),
-    ]);
-  }
+    use HasStoreAction, HasDeleteAction, HasUpdateAction, HasPublishedAction, HasMoveAction;
 
-  public function editAction(): void {
-    $this->renderPage([
-      'page' => 'important_posts/edit',
-      'data' => $this->getSingleData(),
-    ]);
-  }
+    public function __construct(
+        public ImportantPostsManagementServiceInterface $service,
+        Request                                         $request,
+        View                                            $view,
+        CsrfMiddleware                                  $csrfMiddleware
+    )
+    {
+        parent::__construct($request, $service, $view, $csrfMiddleware);
+    }
 
-  public function createAction(): void {
-    $this->renderPage([
-      'page' => 'important_posts/create',
-    ]);
-  }
+    public function indexAction(): void
+    {
+        $this->renderPage([
+            'page' => 'important_posts/index',
+            'data' => $this->service->getAllImportantPosts(),
+        ]);
+    }
 
-  public function showAction(): void {
-    $this->renderPage([
-      'page' => 'important_posts/show',
-      'data' => $this->getSingleData(),
-    ]);
-  }
+    /**
+     * @throws NotFoundException
+     */
+    public function editAction(): void
+    {
+        $this->renderPage([
+            'page' => 'important_posts/edit',
+            'data' => $this->getSingleData(),
+        ]);
+    }
 
-  public function confirmDeleteAction(): void {
-    $this->renderPage([
-      'page' => 'important_posts/delete',
-      'data' => $this->getSingleData(),
-    ]);
-  }
+    public function createAction(): void
+    {
+        $this->renderPage([
+            'page' => 'important_posts/create',
+        ]);
+    }
 
-  protected function getModuleName(): string {
-    return 'important_posts';
-  }
+    /**
+     * @throws NotFoundException
+     */
+    public function showAction(): void
+    {
+        $this->renderPage([
+            'page' => 'important_posts/show',
+            'data' => $this->getSingleData(),
+        ]);
+    }
 
-  protected function getDataToCreate(): array {
-    return $this->getPostDataToCreate();
-  }
+    /**
+     * @throws NotFoundException
+     */
+    public function confirmDeleteAction(): void
+    {
+        $this->renderPage([
+            'page' => 'important_posts/delete',
+            'data' => $this->getSingleData(),
+        ]);
+    }
 
-  protected function getDataToUpdate(): array {
-    return $this->getPostDataToEdit();
-  }
+    protected function getModuleName(): string
+    {
+        return 'important_posts';
+    }
 
-  protected function handleCreate(array $data): void {
-    $this->service->createImportantPost($data);
-  }
+    protected function getDataToCreate(): array
+    {
+        return $this->getPostDataToCreate();
+    }
 
-  protected function handleUpdate(array $data): void {
-    $this->service->updateImportantPost($data);
-  }
+    protected function getDataToUpdate(): array
+    {
+        return $this->getPostDataToEdit();
+    }
 
-  protected function handleDelete(int $id): void {
-    $this->service->deleteImportantPost($id);
-  }
+    protected function handleCreate(array $data): void
+    {
+        $this->service->createImportantPost($data);
+    }
 
-  protected function handlePublish(array $data): void {
-    $this->service->publishedImportantPost($data);
-  }
+    protected function handleUpdate(array $data): void
+    {
+        $this->service->updateImportantPost($data);
+    }
 
-  protected function handleMove(array $data): void {
-    $this->service->moveImportantPost($data);
-  }
+    protected function handleDelete(int $id): void
+    {
+        $this->service->deleteImportantPost($id);
+    }
+
+    protected function handlePublish(array $data): void
+    {
+        $this->service->publishedImportantPost($data);
+    }
+
+    protected function handleMove(array $data): void
+    {
+        $this->service->moveImportantPost($data);
+    }
 }
