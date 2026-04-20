679c3ed42a9eb77197d47d1cfa33a013f4446dba
KrzysztofKuklinski2000 <zono.mrzono21@gmail.com>
2026-04-20 21:34:28 +0200

    refactor: update ContactController to use modular action traits
    Applied new architecture to ContactController:
    
    * Integrated HasUpdateAction.

--- src/Controller/Dashboard/ContactController.php
+++ src/Controller/Dashboard/ContactController.php
@@ -2,41 +2,45 @@
 
 namespace App\Controller\Dashboard;
 
+use App\Controller\Dashboard\Traits\HasUpdateAction;
 use App\View;
 use App\Core\Request;
 use App\Middleware\CsrfMiddleware;
 use App\Service\Dashboard\ContactManagementServiceInterface;
 
-class ContactController extends AbstractDashboardController {
-  public function __construct(
-    public ContactManagementServiceInterface $service,
-    Request $request,
-    View $view,
-    CsrfMiddleware $csrfMiddleware
-  ) {
-    parent::__construct($request, $service, $view, $csrfMiddleware);
-  }
+class ContactController extends AbstractDashboardController
+{
+    use HasUpdateAction;
+    public function __construct(
+        public ContactManagementServiceInterface $service,
+        Request                                  $request,
+        View                                     $view,
+        CsrfMiddleware                           $csrfMiddleware
+    )
+    {
+        parent::__construct($request, $service, $view, $csrfMiddleware);
+    }
 
-  protected function getModuleName(): string
-  {
-    return 'contact';
-  }
+    protected function getModuleName(): string
+    {
+        return 'contact';
+    }
 
-  public function editAction(): void
-  {
-    $this->renderPage([
-      'page' => 'contact/edit',
-      'data' => $this->service->getContact(),
-    ]);
-  }
+    public function editAction(): void
+    {
+        $this->renderPage([
+            'page' => 'contact/edit',
+            'data' => $this->service->getContact(),
+        ]);
+    }
 
-  protected function getDataToUpdate(): array
-  {
-    return $this->getDataToContactEdit();
-  }
+    protected function getDataToUpdate(): array
+    {
+        return $this->getDataToContactEdit();
+    }
 
-  protected function handleUpdate(array $data): void
-  {
-    $this->service->updateContact($data);
-  }
+    protected function handleUpdate(array $data): void
+    {
+        $this->service->updateContact($data);
+    }
 }
