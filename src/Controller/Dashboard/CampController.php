0dfde2dc8cf0e0145cbc45069270a23f8e652da3
KrzysztofKuklinski2000 <zono.mrzono21@gmail.com>
2026-04-21 18:28:54 +0200

    refactor: update CampController to use modular action traits
    Applied new architecture to CampController:
    
    * Integrated HasUpdateAction.

--- src/Controller/Dashboard/CampController.php
+++ src/Controller/Dashboard/CampController.php
@@ -2,37 +2,45 @@
 
 namespace App\Controller\Dashboard;
 
+use App\Controller\Dashboard\Traits\HasUpdateAction;
 use App\View;
 use App\Core\Request;
 use App\Middleware\CsrfMiddleware;
 use App\Service\Dashboard\CampManagementServiceInterface;
 
-class CampController extends AbstractDashboardController {
-  public function __construct(
-    public CampManagementServiceInterface $service,
-    Request $request,
-    View $view,
-    CsrfMiddleware $csrfMiddleware
-  ) {
-    parent::__construct($request, $service, $view, $csrfMiddleware);
-  }
+class CampController extends AbstractDashboardController
+{
+    use HasUpdateAction;
+    public function __construct(
+        public CampManagementServiceInterface $service,
+        Request                               $request,
+        View                                  $view,
+        CsrfMiddleware                        $csrfMiddleware
+    )
+    {
+        parent::__construct($request, $service, $view, $csrfMiddleware);
+    }
 
-  public function editAction(): void {
-    $this->renderPage([
-      'page' => 'camp/edit',
-      'data' => $this->service->getCamp(),
-    ]);
-  }
+    public function editAction(): void
+    {
+        $this->renderPage([
+            'page' => 'camp/edit',
+            'data' => $this->service->getCamp(),
+        ]);
+    }
 
-  protected function getModuleName(): string {
-    return 'camp';
-  }
+    protected function getModuleName(): string
+    {
+        return 'camp';
+    }
 
-  protected function getDataToUpdate(): array {
-    return $this->getDataToCampEdit();
-  }
+    protected function getDataToUpdate(): array
+    {
+        return $this->getDataToCampEdit();
+    }
 
-  protected function handleUpdate(array $data): void {
-    $this->service->updateCamp($data);
-  }
+    protected function handleUpdate(array $data): void
+    {
+        $this->service->updateCamp($data);
+    }
 }
