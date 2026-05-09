6b7c87d920290ab910d91f094da5b34328108f12
KrzysztofKuklinski2000 <zono.mrzono21@gmail.com>
2026-05-09 16:45:39 +0200

    refactor(core): implement lazy initialization for database connection
    * Add private $connection property to Database class
    * Modify connect() to reuse the existing PDO instance if available
    * Prevent redundant database connections to improve application performance

--- src/Core/Database.php
+++ src/Core/Database.php
@@ -6,24 +6,39 @@ use PDO;
 use PDOException;
 use App\Exception\RepositoryException;
 
-class Database {
-  public function __construct(private array $config){}
-
-  public function connect(): PDO {
-    try {
-      $dsn = "mysql:dbname={$this->config['database']};host={$this->config['host']}";
-
-      return $this->createConnection($dsn, $this->config['user'], $this->config['password']);
-    } catch (PDOException $e) {
-      throw new RepositoryException('Błąd połączenia z bazą danych !', 500, $e);
+class Database
+{
+    private ?PDO $connection = null;
+    public function __construct(private readonly array $config)
+    {
     }
-  }
 
-  protected function createConnection(string $dsn, string $user, string $password): PDO {
-    return new PDO($dsn, $user, $password, [
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-      PDO::ATTR_PERSISTENT => false,
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    ]);
-  }
+    /**
+     * @throws RepositoryException
+     */
+    public function connect(): PDO
+    {
+        if ($this->connection !== null) {
+            return $this->connection;
+        }
+
+        try {
+            $dsn = "mysql:dbname={$this->config['database']};host={$this->config['host']}";
+
+            $this->connection = $this->createConnection($dsn, $this->config['user'], $this->config['password']);
+
+            return $this->connection;
+        } catch (PDOException $e) {
+            throw new RepositoryException('Błąd połączenia z bazą danych !', 500, $e);
+        }
+    }
+
+    protected function createConnection(string $dsn, string $user, string $password): PDO
+    {
+        return new PDO($dsn, $user, $password, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_PERSISTENT => false,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
 }
